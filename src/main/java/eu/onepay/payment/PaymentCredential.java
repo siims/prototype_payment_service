1912dad7c8683b09fa9225bc0a4a4c6b44b9be11
mihkel Lukats <mihke.lukats@gmail.com>
2015-09-04 12:30:51 +0300

    empty readme

--- src/main/java/eu/onepay/payment/PaymentCredential.java
+++ src/main/java/eu/onepay/payment/PaymentCredential.java
@@ -1,17 +1,9 @@
 package eu.onepay.payment;
 
-public class PaymentCredential {
+public interface PaymentCredential {
     
     public static final String CUSTOM_METHODS_KEY = "customPaymentMethods";
 
-    private Long paymentId = 0L;
-
-    public Long getPaymentId() {
-        return paymentId;
-    }
-
-    public void setPaymentId(Long paymentId) {
-        this.paymentId = paymentId;
-    }
+    public Long getPaymentId();
 
 }
