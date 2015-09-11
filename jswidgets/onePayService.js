920c0b63ba5edc35440379efce5221c4588e46e8
mihkel Lukats <mihke.lukats@gmail.com>
2015-09-11 14:48:45 +0300

    added support for form submit

--- jswidgets/onePayService.js
+++ jswidgets/onePayService.js
@@ -1,58 +1,12 @@
-jQuery(document).ready(function($) {
-	onepay.getPayMethods();
-
-});
-var onepay = {
-		data: {
-			merchant_id: "",
-			pay_option: "",
-			order_id: "",
-			amount: "",
-			reference_no: "",
-			explanation: ""
-		},
-		dataParams: ["merchant_id", "pay_option", "order_id", "amount", "reference_no", "explanation"],
-		add: function(fieldName, fieldvalue) {
-			this.data[fieldName] = fieldvalue;
-		},
-		submitForm: function() {
-			var form = $(form);
-			form.css("display", "none");
-			form.css("visibility", "hidden");
-			form.appendTo("body").submit();
-		},
-
-		doCall: function() {
-			$.ajax({
-					url: "http://localhost:8080/onepay_bankpayment/pay",
-					dataType: 'jsonp',
-					jsonpCallback: 'jsonpCallback',
-					crossDomain: true,
-					data: "json_data=" + JSON.stringify(this.data, this.dataParams
-					),
-				success: successCallback,
-				error: errorCallback
-			});
-	},
-	getPayMethods: function() {
-		$.ajax({
-			url: "http://localhost:8080/onepay_bankpayment/get/paymethods/merchant/1234",
-			type: "POST",
-			dataType: 'jsonp',
-			jsonpCallback: 'jsonpCallback',
-			data: "",
-			crossDomain: true,
-			success: paymethodsSuccess,
-			error: errorCallback
-		});
-	}
-
-}
 var paymethodsSuccess = function(response) {
 
 	$("body").append(JSON.stringify(response));
 	showMethodsInList(response);
 
+}
+
+function submitForm() {
+	onepay.submitForm();
 }
 
 function showMethodsInList(resp) {
@@ -69,11 +23,12 @@ function getListRow(obj) {
 	var img = $("<img src='" + obj.imageUrl + "'>");
 	li.append(img);
 	li.append(obj.paymentId);
-	li.click(function () {
+	li.click(function() {
 		makePayment(obj.paymentId);
 	});
 	return li;
 }
+
 
 function makePayment(paymentId) {
 	onepay.data.pay_option = paymentId;
@@ -101,4 +56,56 @@ function inputLenghtToContentSize() {
 	$("input[NAME^='VK_']").each(function() {
 		$(this).attr("size", $(this).val().length);
 	});
+}
+
+
+
+jQuery(document).ready(function($) {
+	onepay.getPayMethods();
+
+});
+var onepay = {
+	data: {
+		merchant_id: "",
+		pay_option: "",
+		order_id: "",
+		amount: "",
+		reference_no: "",
+		explanation: ""
+	},
+	dataParams: ["merchant_id", "pay_option", "order_id", "amount", "reference_no", "explanation"],
+	add: function(fieldName, fieldvalue) {
+		this.data[fieldName] = fieldvalue;
+	},
+	submitForm: function() {
+		form = $(form);
+		form.css("display", "none");
+		form.css("visibility", "hidden");
+		form.appendTo("body").submit();
+	},
+
+	doCall: function() {
+		$.ajax({
+			url: "http://localhost:8080/onepay_bankpayment/pay",
+			dataType: 'jsonp',
+			jsonpCallback: 'jsonpCallback',
+			crossDomain: true,
+			data: "json_data=" + JSON.stringify(this.data, this.dataParams),
+			success: successCallback,
+			error: errorCallback
+		});
+	},
+	getPayMethods: function() {
+		$.ajax({
+			url: "http://localhost:8080/onepay_bankpayment/get/paymethods/merchant/1234",
+			type: "POST",
+			dataType: 'jsonp',
+			jsonpCallback: 'jsonpCallback',
+			data: "",
+			crossDomain: true,
+			success: paymethodsSuccess,
+			error: errorCallback
+		});
+	}
+
 }
