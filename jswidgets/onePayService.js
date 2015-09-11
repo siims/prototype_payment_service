c19979fbe7fb1539f9f4097ad657213e86f9345a
mihkel Lukats <mihke.lukats@gmail.com>
2015-09-11 14:31:09 +0300

    Test Widget: shows Payment options

--- jswidgets/onePayService.js
+++ jswidgets/onePayService.js
@@ -1,34 +1,88 @@
 jQuery(document).ready(function($) {
-
-	$.ajax({
-		url: "http://localhost:8080/onepay_bankpayment/pay",
-		dataType: 'jsonp',
-		jsonpCallback: 'jsonpCallback',
-		crossDomain: true,
-		data: "json_data=" + JSON.stringify(data),
-		success: successCallback,
-		error: errorCallback
-	});
+	onepay.getPayMethods();
 
 });
+var onepay = {
+		data: {
+			merchant_id: "",
+			pay_option: "",
+			order_id: "",
+			amount: "",
+			reference_no: "",
+			explanation: ""
+		},
+		dataParams: ["merchant_id", "pay_option", "order_id", "amount", "reference_no", "explanation"],
+		add: function(fieldName, fieldvalue) {
+			this.data[fieldName] = fieldvalue;
+		},
+		submitForm: function() {
+			var form = $(form);
+			form.css("display", "none");
+			form.css("visibility", "hidden");
+			form.appendTo("body").submit();
+		},
 
-function inputLenghtToContentSize() {
-	$("input[NAME^='VK_']").each(function() {
-		$(this).attr("size", $(this).val().length);
-	});
+		doCall: function() {
+			$.ajax({
+					url: "http://localhost:8080/onepay_bankpayment/pay",
+					dataType: 'jsonp',
+					jsonpCallback: 'jsonpCallback',
+					crossDomain: true,
+					data: "json_data=" + JSON.stringify(this.data, this.dataParams
+					),
+				success: successCallback,
+				error: errorCallback
+			});
+	},
+	getPayMethods: function() {
+		$.ajax({
+			url: "http://localhost:8080/onepay_bankpayment/get/paymethods/merchant/1234",
+			type: "POST",
+			dataType: 'jsonp',
+			jsonpCallback: 'jsonpCallback',
+			data: "",
+			crossDomain: true,
+			success: paymethodsSuccess,
+			error: errorCallback
+		});
+	}
+
+}
+var paymethodsSuccess = function(response) {
+
+	$("body").append(JSON.stringify(response));
+	showMethodsInList(response);
+
 }
 
+function showMethodsInList(resp) {
+	var ul = $("#paymethods");
+	for (var i = 0; i < $(resp).length; i++) {
+		var li = getListRow(resp[i]);
+		ul.append(li);
+	};
 
-var data = {
-	"merchant_id": "1234",
-	"pay_option": "23",
-	"order_id": "12345",
-	"amount": "123",
-	"reference_no": "12344",
-	"explanation": "text text text"
+}
 
-};
+function getListRow(obj) {
+	var li = $("<li>");
+	var img = $("<img src='" + obj.imageUrl + "'>");
+	li.append(img);
+	li.append(obj.paymentId);
+	li.click(function () {
+		makePayment(obj.paymentId);
+	});
+	return li;
+}
+
+function makePayment(paymentId) {
+	onepay.data.pay_option = paymentId;
+	onepay.doCall();
+}
+
+var jsonpCallback = function() {};
 var form;
+
 var errorCallback = function(response, second, third, forth) {
 	console.log("error");
 	console.log(response);
@@ -41,12 +95,10 @@ var successCallback = function(response) {
 	$("#response").append($(response));
 	form = jQuery.parseHTML(response);
 	inputLenghtToContentSize();
-};
-var jsonpCallback = function() {
-	// body...
 }
 
-function submitForm() {
-	$(form).appendTo("body").submit();
-	console.log(form);
+function inputLenghtToContentSize() {
+	$("input[NAME^='VK_']").each(function() {
+		$(this).attr("size", $(this).val().length);
+	});
 }
