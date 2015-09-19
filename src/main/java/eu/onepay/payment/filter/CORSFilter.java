5db83c754d101044f26484079f1644c82b415c37
mihkel Lukats <mihke.lukats@gmail.com>
2015-09-19 17:47:28 +0300

    \@Web* annotatsion added (Did it in last commit as well)

--- src/main/java/eu/onepay/payment/filter/CORSFilter.java
+++ src/main/java/eu/onepay/payment/filter/CORSFilter.java
@@ -1,10 +1,13 @@
 package eu.onepay.payment.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
+@WebFilter("*")
 public class CORSFilter implements Filter {
 
     @Override
